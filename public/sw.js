67b6d801dcaccfa4a1f8840b50964f8e48bf7b78
Andrew Zeller <andrew.j.zeller@gmail.com>
2018-12-30 10:55:20 -0800

    adds service worker

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,30 @@
-w
+// service-worker.js
+
+console.log('Hello from sw.js');
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
+workbox.routing.registerRoute(
+    new RegExp('.*\.js'),
+    workbox.strategies.networkFirst()
+);
+
+workbox.routing.registerRoute(
+  new RegExp('.*\.css'),
+  workbox.strategies.networkFirst()
+);
+
+// Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
+workbox.routing.registerRoute(
+    /^https:\/\/fonts\.googleapis\.com/,
+    workbox.strategies.staleWhileRevalidate({
+      cacheName: 'google-fonts-stylesheets',
+    })
+);
+
